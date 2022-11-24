27450a1eaf0d60a8530c58c96fb0ec897a2a6698
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2022-11-24 17:45:01 +0300

    auth

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/Unauthorized');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    next(new Unauthorized('Необходима авторизация'));
+    return;
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+    );
+  } catch (err) {
+    next(new Unauthorized('Необходима авторизация'));
+    return;
+  }
+
+  req.user = payload;
+
+  next();
+};
